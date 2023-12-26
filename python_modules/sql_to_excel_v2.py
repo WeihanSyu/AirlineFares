435fd45ca1780bedf6581662b9b02f91a699aa9b
Weihan <weihansyu@gmail.com>
2023-12-25 20:09:39 -0800

    Changed function from adding a wide 'JOIN' table combining all the different website flight tickets to a vertical 'Union' table of the tickets which should make Tableau visualization easier

--- python_modules/sql_to_excel_v2.py
+++ python_modules/sql_to_excel_v2.py
@@ -4,49 +4,78 @@ import re
 
 def sql_excel(conn):
 
-    sql = "SELECT * FROM all_ticket;"
+    # Upload expedia table to Excel
+    sql = "SELECT date_scrape, airline, ticket_type, ticket_class, origin, destination,\
+        going_stops, going_date, going_time, going_arrive_time, going_travel_time, return_date, price\
+        FROM expedia;"
     df = pd.read_sql(sql, conn)
-
-    price_index_expedia = df.columns.get_loc('e_price')
-    for i in range(len(df['e_price'])):
-        if df['e_price'][i] != None:
-            x = re.findall('\$.*', df['e_price'][i])[0][1:]
+    
+    col_website = ['expedia']*len(df['price'])
+    df.insert(0, 'website', col_website, allow_duplicates=True)
+    
+    price_index_expedia = df.columns.get_loc('price')
+    for i in range(len(df['price'])):
+        if df['price'][i] != None:
+            x = re.findall('\$.*', df['price'][i])[0][1:]
             y = int(re.sub(',','',x))
             df.iloc[i, price_index_expedia] = y
         else:
             pass
-        
-    price_index_kayak = df.columns.get_loc('k_price')
-    for i in range(len(df['k_price'])):
-        if df['k_price'][i] != None:
-            x = re.findall('\$.*', df['k_price'][i])[0][1:]
+
+    try:
+        existing_df = pd.read_excel('airfare_tables.xlsx', sheet_name='tickets')
+        existing_df = existing_df.drop(columns=(['id']))
+        df = pd.concat([existing_df, df], ignore_index=True)
+
+        df['going_date'] = pd.to_datetime(df['going_date']).dt.date
+        df['return_date'] = pd.to_datetime(df['return_date']).dt.date
+
+        with pd.ExcelWriter('airfare_tables.xlsx', 
+                        mode='a',
+                        if_sheet_exists='replace',
+        ) as writer:
+            df.to_excel(writer, sheet_name='tickets', index=True, index_label='id')
+    except:
+        with pd.ExcelWriter('airfare_tables.xlsx', 
+                        mode='w',
+        ) as writer:
+            df.to_excel(writer, sheet_name='tickets', index=True, index_label='id')
+
+    
+    # Upload kayak table to Excel
+    sql = "SELECT date_scrape, airline, ticket_type, ticket_class, origin, destination,\
+        going_stops, going_date, going_time, going_arrive_time, going_travel_time,\
+        return_stops, return_date, return_time, return_arrive_time, return_travel_time, price\
+        FROM kayak;"
+    df = pd.read_sql(sql, conn)
+
+    col_website = ['kayak']*len(df['price'])
+    df.insert(0, 'website', col_website, allow_duplicates=True)
+    
+    price_index_kayak = df.columns.get_loc('price')
+    for i in range(len(df['price'])):
+        if df['price'][i] != None:
+            x = re.findall('\$.*', df['price'][i])[0][1:]
             y = int(re.sub(',','',x))
             df.iloc[i, price_index_kayak] = y
         else:
             pass
 
     try:
-        existing_df = pd.read_excel('airfare_tables_2.xlsx', sheet_name='all_ticket')
+        existing_df = pd.read_excel('airfare_tables.xlsx', sheet_name='tickets')
         existing_df = existing_df.drop(columns=(['id']))
         df = pd.concat([existing_df, df], ignore_index=True)
-        
-        # I don't why our "date" type columns sometimes change to "datetime" after concatenation
-        df['e_going_date'] = pd.to_datetime(df['e_going_date']).dt.date
-        df['e_return_date'] = pd.to_datetime(df['e_return_date']).dt.date
-        df['k_going_date'] = pd.to_datetime(df['k_going_date']).dt.date
-        df['k_return_date'] = pd.to_datetime(df['k_return_date']).dt.date
-        
-        with pd.ExcelWriter('airfare_tables_2.xlsx', 
+
+        df['going_date'] = pd.to_datetime(df['going_date']).dt.date
+        df['return_date'] = pd.to_datetime(df['return_date']).dt.date
+
+        with pd.ExcelWriter('airfare_tables.xlsx', 
                         mode='a',
                         if_sheet_exists='replace',
         ) as writer:
-            df.to_excel(writer, sheet_name='all_ticket', index=True, index_label='id')
+            df.to_excel(writer, sheet_name='tickets', index=True, index_label='id')
     except:
-        with pd.ExcelWriter('airfare_tables_2.xlsx', 
+        with pd.ExcelWriter('airfare_tables.xlsx', 
                         mode='w',
         ) as writer:
-            df.to_excel(writer, sheet_name='all_ticket', index=True, index_label='id')
-        
-    conn.execute(text("DROP TABLE dbo.all_ticket"))
-
-
+            df.to_excel(writer, sheet_name='tickets', index=True, index_label='id')
